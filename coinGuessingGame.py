9d118c9665f13f3e37f4af82dd273ead6a9b0bf0
Tracy Han <hyn2000219@gmail.com>
2014-11-16 23:03:28 -0500

    Initial program and highest score

---
+++ coinGuessingGame.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# This project is a easy guessing game. Users will guess the side of the coin and the game
+# can keep the highest score.
+
+from random import choice
+
+def coin_flip():
+	coinSide = choice(["heads", "tails"])
+	return coinSide
+
+# --------------------- Main Program Below ------------------------
+f = open("highestScore.txt", "r+")
+highestScore = f.read()
+
+print("This is a coin guessing game. All time highest score: {}.".format(highestScore))
+score = 0
+
+while True:
+	guess = raw_input("Please predict heads or tails ")
+	coinSide = coin_flip()
+	guess = guess[0].lower()
+	
+	if guess != 'h' and guess != 't':
+		print("Every coin only have two sides, heads and tails.")
+		break
+	
+	if guess == coinSide[0]:
+		score += 1
+		print("That\'s right! +1!!! Present score: {}".format(score))
+	else:
+		print("That\'s wrong. Game over. Good luck next time. Final score: {}".format(score))
+		break
+	
+if score >= int(highestScore):
+	f.seek(0)
+	f.write(str(score))
+	print("Congratulations, you break the records!!! New highest score: {}".format(score))
+			
+		
